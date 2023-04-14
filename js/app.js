1a77f0146964bf02eca42ea820580d487d073ee6
Miguel Ruiz <later481@gmail.com>
2023-04-13 22:47:26 -0600

    funcion submit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const d = document;
 //VARIABLES
 const formulario = d.querySelector("#formulario");
-const tarea = d.querySelector("#tarea");
 let tareas = [];
 
 
@@ -13,5 +12,25 @@ function eventListeners(){
 //FUNCIONES
 function agregarTarea(e){
     e.preventDefault();
-    console.log("Agregando Tarea")
+    const tarea = d.querySelector("#tarea_input").value;
+
+    //VALIDACION
+    if(tarea === ""){
+        mostrarError("Una tarea no puede ir vacio");
+        return;
+    }
+}
+
+function mostrarError(error){
+    const mensajeError = d.createElement("p")
+    mensajeError.textContent = error;
+    mensajeError.classList.add("error");
+    
+    //Insertar en el contenido
+    const contenido = d.querySelector("#contenido");
+    contenido.appendChild(mensajeError);
+
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
 }
